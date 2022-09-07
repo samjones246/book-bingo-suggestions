38d53082df4337a3c8ff0571eb314a2cdb566603
Sam Jones <sam.jones240699@gmail.com>
2022-09-07 01:07:50 +0100

    feat: track selected prompt

--- src/app/prompts-list/prompts-list.component.ts
+++ src/app/prompts-list/prompts-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { BingoInfoService } from '../bingos/bingo-info.service';
 import { Bingo } from '../models/bingo.model';
+import { Prompt } from '../models/prompt.model';
 
 @Component({
   selector: 'app-prompts-list',
@@ -10,10 +11,25 @@ import { Bingo } from '../models/bingo.model';
 export class PromptsListComponent implements OnInit {
 
   bingo: Bingo
+  selectedPrompt: Prompt | null = null
+  selectedIndex: number | null = null
+  selectedElem: EventTarget | null = null
 
-  constructor(bingoInfoService: BingoInfoService) {
+  constructor(bingoInfoService: BingoInfoService, private renderer: Renderer2) {
     this.bingo = bingoInfoService.getBingoInfo()
   }
 
   ngOnInit(): void { }
+
+  onPromptClicked(event: Event, index: number) {
+    if (this.selectedIndex != index) {
+      this.selectedIndex = index
+      this.selectedPrompt = this.bingo.prompts[index]
+      if (this.selectedElem != null) {
+        this.renderer.removeClass(this.selectedElem, "pure-menu-selected")
+      }
+      this.selectedElem = event.target
+      this.renderer.addClass(this.selectedElem, "pure-menu-selected")
+    }
+  }
 }
