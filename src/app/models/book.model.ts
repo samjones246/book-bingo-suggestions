2b1f5c24bd3bbd66e600bc11777b0b1a2830d711
Sam Jones <sam.jones240699@gmail.com>
2022-10-13 14:24:08 +0100

    feat: added isbn property for books

--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,6 +1,7 @@
 export class Book {
     constructor(
         public title: String,
-        public author: String
+        public author: String,
+        public isbn: String | null = null
     ) {}
 }
--- src/app/prompts-list/prompts-list.component.ts
+++ src/app/prompts-list/prompts-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { docData, Firestore } from '@angular/fire/firestore';
 import { doc } from '@firebase/firestore';
 import { Bingo } from '../models/bingo.model';
+import { Book } from '../models/book.model';
 import { Prompt } from '../models/prompt.model';
 
 const ACTIVE_BINGO_ID = "Q6ZoKH3DtdaSz0ygA5Si"
@@ -38,4 +39,11 @@ export class PromptsListComponent implements OnInit {
     return index == this.selected
   }
 
+  isbnString(book: Book) {
+    if (book.isbn == null) {
+      return ""
+    } else {
+      return "(" + book.isbn + ")"
+    }
+  }
 }
