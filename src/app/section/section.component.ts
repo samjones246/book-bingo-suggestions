71163d94a49c8e5e6ef80396e9ac2c86d2ec9891
Sam Jones <sam.jones240699@gmail.com>
2022-10-15 00:42:39 +0100

    feat: handle isbn

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
 import { Book } from '../models/book.model';
 
 @Component({
@@ -7,17 +6,11 @@ import { Book } from '../models/book.model';
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss']
 })
-export class SectionComponent implements OnInit {
+export class SectionComponent {
   @Input() description: String = "A Prompt"
   @Input() books: Book[] = [new Book("Floop Doop", "Writey Writer")]
   @Input() expanded: Boolean = false
   @Output() select: EventEmitter<any> = new EventEmitter()
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
 
   toggle(): void {
     this.select.emit(null)
